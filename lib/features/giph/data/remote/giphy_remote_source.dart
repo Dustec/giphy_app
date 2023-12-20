cd4783b5f1678025292a7e63fef4f46a132230aa
Héctor Alvarez <halvarezr@outlook.com>
2023-12-20 14:56:04 -0600

    implement remote source

--- lib/features/giph/data/remote/giphy_remote_source.dart
+++ lib/features/giph/data/remote/giphy_remote_source.dart
@@ -1,12 +1,26 @@
 import 'package:giphy_app/core/config/giphy_api.dart';
+import 'package:giphy_app/core/domain/contracts/mapper.dart';
+import 'package:giphy_app/core/domain/managers/http_manager.dart';
+import 'package:giphy_app/features/giph/data/mappers/gif_from_dto_mapper.dart';
+import 'package:giphy_app/features/giph/data/models/gif_dto.dart';
+import 'package:giphy_app/features/giph/data/models/gif_request_dto.dart';
 import 'package:giphy_app/features/giph/domain/models/gif_model.dart';
 import 'package:giphy_app/features/giph/domain/sources/giphy_source.dart';
 
 class GiphyRemoteSource with GiphyApi implements GiphySource {
+  GiphyRemoteSource({
+    this.giphMapper = const GifFromDtoMapper(),
+  });
+
+  final Mapper<GifDto, GifModel> giphMapper;
+
   @override
   Stream<List<GifModel>> getGiphs({
     String? searchText,
   }) async* {
-    // yield* http.get('/v1/gifs/trending')`
+    yield* http
+        .get('/v1/gifs/trending')
+        .handle(mapper: (json) => GifRequestDto.fromJson(json))
+        .map((dto) => dto.data?.map((e) => giphMapper.map(e)).toList() ?? []);
   }
 }
